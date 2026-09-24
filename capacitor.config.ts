258c7e4658cc6691b0d0c2514658e84f505be771
Jonysued <jonatan@rimonim.com.ar>
2026-09-24 09:12:44 -0300

    Prepare Android build and dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "app.rimonim.empaque",
+  appName: "Rimonim Empaque",
+  webDir: "dist",
+  server: { androidScheme: "https" },
+};
+
+export default config;
